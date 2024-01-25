b7f1544fb67325e0ef88add2c1c81b5f33d7b24b
HusseinA7 <xuseenarab7@gmail.com>
2024-01-25 16:56:25 +0300

    Flutter MyTodo App

---
+++ README.md
@@ -0,0 +1,4 @@
+# Flutter My ToDo App
+
+Hey my name is Hussein and i developed a basic to-do app built with Flutter. This app has add/delete/update functinality with search for any existing to-do item in the app.
+
---
+++ lib/model/todo.dart
@@ -0,0 +1,22 @@
+class ToDo {
+  String? id;
+  String? todoText;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.todoText,
+    this.isDone = false,
+  });
+
+  static List<ToDo> todoList() {
+    return [
+      ToDo(id: '01', todoText: 'Morning Excercise', isDone: true ),
+      ToDo(id: '02', todoText: 'Reading quran for 1 hour', isDone: true ),
+      ToDo(id: '03', todoText: 'Check Emails', ),
+      ToDo(id: '04', todoText: 'Team Meeting', ),
+      ToDo(id: '05', todoText: 'Work on mobile apps for 2 hour', ),
+      ToDo(id: '06', todoText: 'Go out with my freids', ),
+    ];
+  }
+}
